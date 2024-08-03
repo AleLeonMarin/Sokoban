51a6b702d699b5d11e0d7c86af69e191bde47f21
Alejandro Leon Marin <aleleonmarin01@gmail.com>
2024-08-02 18:12:49 -0600

    [INIT] structre

--- src/main/java/cr/ac/una/datos/util/BindingUtils.java
+++ src/main/java/cr/ac/una/datos/util/BindingUtils.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cr.ac.una.tarea.util;
+package cr.ac.una.datos.util;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.value.ChangeListener;
