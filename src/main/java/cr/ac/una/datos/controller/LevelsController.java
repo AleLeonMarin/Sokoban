c05e1e19b4449245e70f7042c9a695660ef90aab
Kendall Fonseca <kendallfh2004@outlook.com>
2024-08-11 12:53:20 -0600

    Update LevelsController.java

--- src/main/java/cr/ac/una/datos/controller/LevelsController.java
+++ src/main/java/cr/ac/una/datos/controller/LevelsController.java
@@ -51,7 +51,7 @@ public class LevelsController extends Controller implements Initializable {
 
     @Override
     public void initialize() {
-        loadBoardFromFile("src/main/resources/cr/ac/una/datos/resources/Levels/easy_level1.txt");
+        loadBoardFromFile("src/main/resources/cr/ac/una/datos/resources/Levels/avanced_level1.txt");
         resizeGridPane(width, height);
         loadGridPane();
         game = new Game(board);
